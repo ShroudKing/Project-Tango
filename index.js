2506a7643c34375cd7a3b354fcca07a38c5ba04f
Franco <weakspicedev@gmail.com>
2022-01-04 20:46:58 -0500

    UI Progression

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-let data = {}
+let data = {};
+
+function AddRow(userData) {
+    const rawRowHTML = `
+    <div class=\'row\'>
+        <div class=\'id\'><span>` + userData.id.toString() + `</span></div>
+        <div class=\'name\'><span>` + userData.name.toString() + `</span></div>
+        <div class=\'credits\'><span>` + userData.credits.toString() + `</span></div>
+    </div>
+    `;
+
+    document.getElementById('six').innerHTML += rawRowHTML;
+}
 
 function CreateUser(id) {
     let attributes = {}
 
     attributes.id = id;
-    attributes.name = 'hmm' + id.toString();
+    attributes.name = 'User' + id.toString();
     attributes.credits = Math.floor(Math.random() * 1000000);
 
     data[id] = attributes;
+    AddRow(attributes);
 }
 
-for (let a = 1; a <= 15; a++) {
+for (let a = 1; a <= 7; a++) {
     CreateUser(a);
 };
 
